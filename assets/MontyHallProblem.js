699960b461b3adf0dde9ed1c69978ad16a4fcfe4
Michael Ouellette <michaelouellette27@hotmail.com>
2022-10-07 10:47:43 -0400

    Second Commit

--- assets/MontyHallProblem.js
+++ assets/MontyHallProblem.js
@@ -9,9 +9,6 @@ class Statistics {
 
     // Percentage between win and losses arrays
     percentageWonNotChangingDoor() {
-        console.log('PCTWonNotChangingDoor');
-        console.log(this.gamesWithSameDoorWon);
-        console.log(this.gamesWithSameDoorLost);
         let percentageWonNotChangingDoor = (this.gamesWithSameDoorWon.length / this.gamesWithSameDoorLost.length) * 100;
         return Math.floor(percentageWonNotChangingDoor);
     }
@@ -19,85 +16,10 @@ class Statistics {
 
     // Percentage between win and losses arrays
     pctWinChangingDoor() {
-        console.log('PCTWinChangingDoor');
-        console.log(this.gamesWithDoorChangeWon)
-        console.log(this.gamesWithDoorChangeLost)
         let pctWinChangingDoor = (this.gamesWithDoorChangeWon.length / this.gamesWithDoorChangeLost.length) * 100;
         return Math.floor(pctWinChangingDoor);
     }
     
-
-}
-
-class Game {
-    constructor(toSwitch) {
-        
-        
-        this.doors = [];
-        
-        this.doors.push(new Door(3,false));
-        this.doors.push(new Door(2, false));
-        this.doors.push(new Door(1, false));
-
-        this.carDoor = this.selectCarDoor();
-        this.doorPicked = toSwitch;
-        this.openedGoatDoor = this.goatDoorPick();
-        this.finalPick = [];
-        
-        this.won;
-        //this.result();
-        // console.log(this.doors)
-        // console.log(this.doorPicked)
-        // console.log(this.openedGoatDoor)
-        // console.log(this.finalPick)
-        
-    }
-
-    // Picks a door at random with a car behind it
-    selectCarDoor() {
-        let carDoorIndex = Math.floor(Math.random() * this.doors.length)
-        let carDoor = this.doors[carDoorIndex];
-        carDoor.isCar = true;
-        return carDoor;
-    }
-
-    // Player pick 
-    playerChoicePick() {
-        let playerChoicePick = Math.floor(Math.random() * this.doors.length);
-        return this.doors[playerChoicePick];
-
-    }
-
-    // Computer door pick with the goat behind it
-    goatDoorPick() {
-        for (let i = 0; i < this.doors.length; i++) {
-           let goat = this.doors[i];
-           if (goat.isCar !== true && goat !== this.doorPicked){
-            goat.opened = true;
-
-            return goat;
-
-           }
-
-        }
-
-    }
-
-    // Push the players random decisions to the Statistics Class arrays
-    result () { 
-        this.finalPick.push(this.doorPicked);
-        this.finalPick.push(this.carDoor);
-        //console.log('finalPick index:');
-        //const randomNum = Math.random();
-        //console.log(randomNum);
-        //console.log(this.finalPick.length);
-        //console.log('------------- X');
-       // console.log('--------- -1');
-        let doorIndex = Math.floor(Math.random() * (this.finalPick.length));
-        //console.log(doorIndex)
-        return this.finalPick[doorIndex];
-    }
-
 
 }
 
@@ -112,54 +34,105 @@ class Door {
 
 }
 
-// Scenario
-// runSimulation();
+class Game {
+    constructor() {
+        this.doors = [];
+        // Push the values to the this.doors array
+        this.doors.push(new Door(3,false));
+        this.doors.push(new Door(2, false));
+        this.doors.push(new Door(1, false));
+        //steps
+        this.carDoor = this.initCarDoor();
+        this.initialPlayerPick = this.initPlayerPick();
+        this.openedGoatDoor = this.goatDoorPick();
+        
+    }
 
-let statistics = new Statistics();
+    // Picks a door at random with a car behind it
+    initCarDoor() {
+        let carDoorIndex = Math.floor(Math.random() * this.doors.length)
+        let carDoor = this.doors[carDoorIndex];
+        carDoor.isCar = true;
+        return carDoor;
+    }
 
-// Run the game x amount of times 
-function runSimulation() {
+    // Player pick 
+    initPlayerPick() {
+        let index = Math.floor(Math.random() * this.doors.length);
+        return this.doors[index];
 
-    let gamesCounter = 5;
-    console.log('NO SWITCH')
-    for (let i = 0; i < gamesCounter; i++) {    
-        let game = new Game(false);
-        let finalChoice = game.result();
-        console.log('finalChoice ', finalChoice);
-        if (finalChoice === game.carDoor && finalChoice === game.doorPicked) {
-            statistics.gamesWithSameDoorWon.push(game);
-        } else if (finalChoice !== game.carDoor && finalChoice === game.doorPicked) {
-            statistics.gamesWithSameDoorLost.push(game);
+    }
+
+    // Computer door pick with the goat behind it
+    goatDoorPick() {
+        for (let i = 0; i < this.doors.length; i++) {
+           let goat = this.doors[i];
+           if (!goat.isCar && goat.number !== this.initialPlayerPick.number){
+            goat.isOpen = true;
+
+            return goat;
+
+           }
+
         }
-    } 
-    console.log(statistics.percentageWonNotChangingDoor() + "% of games were won when not switching door.");
 
-    console.log('SWITCH')
-    for (let i = 0; i < gamesCounter; i++) {
-        let game = new Game(true);
-        let finalChoice = game.result();
-        console.log('finalChoice ', finalChoice);    
-        if (finalChoice === game.carDoor && finalChoice !== game.doorPicked){
-            // gamesWithDoorChangeWon.length = gamesCounter
-            statistics.gamesWithDoorChangeWon.push(game);
-        } else if (finalChoice !== game.carDoor && finalChoice === game.doorPicked){
-            // gamesWithDoorChangeLost.length === 0
-            statistics.gamesWithDoorChangeLost.push(game);
+    }
+
+    // Simulate the switch door decision
+    switchDoor() {
+        for (let door of this.doors){
+            if(door.number !== this.initialPlayerPick.number && !door.isOpen){
+                return door;
+            }
         }
     }
-    console.log(statistics.pctWinChangingDoor() + "% of games were won when switching door.");
+    
+    // Result for switch
+    result (toSwitch) { 
+        if(toSwitch){
+            this.finalDoor = this.switchDoor();
+        } else {
+            this.finalDoor = this.initialPlayerPick;
+        }
+    }
+
 }
-runSimulation();
-// let statistics = new Statistics();
-//console.log(statistics.gamesWithSameDoorWonSum)
-//let percentageWonNotChangingDoor = (statistics.gamesWithSameDoorWon.length / statistics.gamesWithSameDoorLostSum.length) * 100;
 
-document.getElementById("demo").innerHTML = statistics.percentageWonNotChangingDoor() + "% of games were won when not switching door.";
+// let statistics = new Statistics(); // < Not necessary, but required by requirements.
+let statistics = new Statistics();
 
-document.getElementById("demo2").innerHTML = statistics.pctWinChangingDoor() + "% of games were won when switching door.";
+// empty global variable to switch their values later on
+let percentWinSwitch;
+let percentWinNoSwitch;
 
+// Function that runs the game x amount of times
+function runGameSimulations(numberOfGames){
+    let switchWinTally = 0;
+    let noSwitchWinTally = 0;
+    for(let i = 0; i < numberOfGames; i++){
+        const game = new Game();
+        // result of same game, when player do switch
+        game.result(true)
+        if(game.finalDoor.isCar){
+            switchWinTally++;
+        }
+        // result of the same game, when player do not switch, 
+        game.result(false)
+        if(game.finalDoor.isCar){
+            noSwitchWinTally++;
+        }
+    }
+    percentWinSwitch = switchWinTally * 100 / numberOfGames;
+    percentWinNoSwitch = noSwitchWinTally * 100 / numberOfGames;
+}
+
+runGameSimulations(100000);
+
+// Html element 
+document.getElementById("demo").innerHTML = percentWinSwitch + "% of games were won when not switching door.";
+document.getElementById("demo2").innerHTML = percentWinNoSwitch + "% of games were won when switching door.";
+
+// Reset the page after pressing the button element
 function myFunction() {
     document.getElementById("myForm").reset();
-
-  }
-
+}
